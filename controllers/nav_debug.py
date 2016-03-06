b3fb2374583124897e23acd00f1a1b8623a6a9ab
Benjamin Drgon <benjamindrgon@gmail.com>
2016-03-06 00:08:03 -0500

    debugger for nav.py, replaces the robot with a human.

---
+++ controllers/nav_debug.py
@@ -0,0 +1,35 @@
+import nav
+#fake robot class for debugging literally everything
+
+
+class FakeRobot:
+    def __init__(self):
+        self.SENSOR_LEFT = "left sensor"
+        self.SENSOR_FRONT = "front sensor"
+        self.SENSOR_RIGHT = "right sensor"
+
+    @staticmethod
+    def step_forward(steps):
+        print("I have just stepped forward " + str(steps))
+        return
+
+    @staticmethod
+    def step_backward(steps):
+        print("I have just stepped backward " + str(steps))
+        return
+
+    @staticmethod
+    def turn_left(amount):
+        print("I have just turned left " + str(amount))
+
+    @staticmethod
+    def turn_right(amount):
+        print("I have just turned right " + str(amount))
+
+    @staticmethod
+    def sense_steps(sensor):
+        steps = raw_input("enter distance to closest wall seens by " + sensor)
+        return int(steps)
+
+robot = FakeRobot()
+navigator = nav.Gps(robot)
