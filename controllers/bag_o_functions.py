116b761b69a4b5c3a3885015f01e94d1de45c434
Shane Parr <dilucidis@gmail.com>
2016-03-07 18:08:52 -0500

    DJK is finished. Works with all test cases so far provided.

--- controllers/bag_o_functions.py
+++ controllers/bag_o_functions.py
@@ -1,5 +1,6 @@
 class bag_o_functions:
-    map = \
+    #this is a test map for display purposes only
+    map =  \
         {
             (0, 0): [
                 [(0, 1), (1, 0)],
@@ -18,6 +19,9 @@ class bag_o_functions:
             ],
             (0, 3): [
                 [(0, 2)],[],[]
+            ],
+            (1, 0):[
+                [(0,0)],[],[]
             ]
         }
     """
@@ -47,30 +51,38 @@ class bag_o_functions:
             # than nothing, so distance of connection is initially set to 'infinity' to fail
         frontier[location] = unvisited.pop(location)
         frontier[location] = [[location], 0]
+        print(unvisited)
         # basic comparison with an actual connection regardless of size of found connection
         # setup complete
         # now use all connections from location node to others to start exploring the unvisited
 
         loc = location
+        print(frontier)
         while True:
             if len(map[loc][0]) > 0:  # there are nodes adjacent and connected to location
-                for t in map[loc][0]:  # iterate through these nodes
-                    if t in unvisited:  # select the node if it is unvisited
-                        if unvisited[t][1] > frontier[loc][1] + 1:
+                for n in map[loc][0]:  # iterate through these nodes
+                    if n in unvisited:  # select the node if it is unvisited
+                        if unvisited[n][1] > frontier[loc][1] + 1:
                             # if its tentative distance is smaller than its assigned distance
-                            unvisited[t][1] = frontier[loc][1] + 1  # replace assigned with tentative
-                            unvisited[t][0] = frontier[loc][0]    # copy over path to frontier to the new node
-                            unvisited[t][0].append(t)           # add the final step of travel to path
+                            unvisited[n][1] = frontier[loc][1] + 1 # replace assigned with tentative
+                            unvisited[n][0]=[]
+                            for i in frontier[loc][0]:
+                                unvisited[n][0].append(i)    # copy over path to frontier to the new node
+                            unvisited[n][0].append(n)           # add the final step of travel to path
                 explored[loc] = frontier.pop(loc)
+                # print(explored[location])
+
+
 
             if target in explored:
-                return explored[target][0]  # when target is reached, end DJk
+                return explored[target] # when target is reached, end DJk
             else:
                 minimum = infinity
-                for t in unvisited:
-                    if unvisited[t][1] < minimum:
-                        loc = t
-                        minimum = unvisited[t][1]
+                for m in unvisited:
+                     if unvisited[m][1] < minimum:
+                         loc = m
+                         minimum = unvisited[m][1]
                 frontier[loc] = unvisited.pop(loc)
         return "fuck"  # return something likely to cause an error if for some reason we escape "while true:"
-    print(DJK((0, 0), map, (0, 1)))
+    print(DJK((0, 0), map, (1, 0))) #use the test map and path to a target (this test works)
+
